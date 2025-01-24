e9b90ecd9a2261e50b350e80cadb96db1face025
thenil3sh <nileshkumar.newdelhi@gmail.com>
2025-01-24 17:22:25 +0530

    first push!

---
+++ build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    let _ = slint_build::compile("./ui/mainWindow.slint");
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use slint::*;
+slint::include_modules!();
+
+fn main() {
+    let window = Main::new().unwrap();
+
+    window.run().unwrap();
+}
